64c3ea007446a0184b468f936251d0f071cae709
henrique montalione <henrique.montalione@gmail.com>
2018-07-23 01:44:52 -0300

    creating controller and assets

---
+++ app/controllers/atractive_controller.rb
@@ -0,0 +1,59 @@
+class AtractiveController < ApplicationController
+  before_action :set_atractive, only: [:show, :edit, :update, :destroy]
+  before_action :allow_without_password, only: [:update]
+
+  def index
+    @atractives = Atractive.all
+  end
+
+  def show
+  end
+
+  def new
+    @atractive = Atractive.new
+  end
+
+  def edit
+  end
+
+  def create
+    @atractive = Atractive.new(atractive_params)
+
+    if @atractive.save
+      redirect_to @atractive, notice: 'Atractive was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @atractive.update(atractive_params)
+      redirect_to @atractive, notice: 'Atractive was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @atractive.destroy
+      redirect_to atractives_path, notice: 'Atractive was successfully destroyed.'
+  end
+
+  private
+
+  def set_atractive
+    @atractive = Atractive.find(params[:id])
+  end
+
+  def atractive_params
+    params.require(:atractive).permit(:name, :address, :duration_time, :max_capacity, :ticket_price)
+  end
+
+  def allow_without_password
+    if params[:atractive][:password].blank? && params[:atractive][:password_confirmation].blank?
+        params[:atractive].delete(:password)
+        params[:atractive].delete(:password_confirmation)
+    end
+  end
+
+end
