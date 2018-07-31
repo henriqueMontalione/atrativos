a184e69dfedb152381fae5266d9d26c299d3924f
henrique montalione <henrique.montalione@gmail.com>
2018-07-31 04:57:35 -0300

    add images validation and finish show view

--- app/models/atractive.rb
+++ app/models/atractive.rb
@@ -3,4 +3,6 @@ class Atractive < ApplicationRecord
   has_attached_file :image_file_name, :styles => { :medium => "300x300>", :thumb => "120x90#" },
   :path => ':rails_root/public/images/atractives/:id-:basename-:style.:extension',
   :url => '/images/atractives/:id-:basename-:style.:extension'
+  validates_attachment_content_type :image_file_name, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
